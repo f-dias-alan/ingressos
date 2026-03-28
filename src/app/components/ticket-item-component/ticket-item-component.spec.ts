8ec12c6a334f438309de512c9f33b65629ecb940
FADOD1 <franciscoalandeoliveira12@egmail.com>
2026-03-28 16:16:12 -0300

    correção nas arquivos de testes

--- src/app/components/ticket-item-component/ticket-item-component.spec.ts
+++ src/app/components/ticket-item-component/ticket-item-component.spec.ts
@@ -1,20 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TicketItemComponent } from './ticket-item-component';
+import { ComponentRef } from '@angular/core';
 
 describe('TicketItemComponent', () => {
   let component: TicketItemComponent;
   let fixture: ComponentFixture<TicketItemComponent>;
+  let componentRef: ComponentRef<TicketItemComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TicketItemComponent]
-    })
-    .compileComponents();
+      imports: [TicketItemComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TicketItemComponent);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    componentRef = fixture.componentRef;
+
+    // 👇 obrigatório: setar o input required antes do detectChanges
+    componentRef.setInput('nomedoEvento', 'Angular Conf 2026');
+
+    fixture.detectChanges();
   });
 
   it('should create', () => {
